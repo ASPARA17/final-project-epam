854b0a4d2ce0e01af35a9a72aa78e47d4058300e
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-16 12:28:33 +0300

    Add Account

---
+++ library/src/main/java/com/epam/jwd/dao/entity/Order.java
@@ -0,0 +1,82 @@
+package com.epam.jwd.dao.entity;
+
+import java.sql.Date;
+
+public class Order extends Entity<Integer>{
+    //TODO: know when use entityId or object of Entity
+    private OrderStatus orderStatus;
+    private Account account;
+    private Book book;
+    private Date dateOfIssue;
+    private Date returnDate;
+    private boolean subscription;
+
+    public Order(Integer id, OrderStatus orderStatus, Account account, Book book,
+                 Date dateOfIssue, Date returnDate, boolean subscription) {
+        super(id);
+        this.orderStatus = orderStatus;
+        this.account = account;
+        this.book = book;
+        this.dateOfIssue = dateOfIssue;
+        this.returnDate = returnDate;
+        this.subscription = subscription;
+    }
+
+    public Order(OrderStatus orderStatus, Account account, Book book, Date dateOfIssue,
+                 Date returnDate, boolean subscription) {
+        this.orderStatus = orderStatus;
+        this.account = account;
+        this.book = book;
+        this.dateOfIssue = dateOfIssue;
+        this.returnDate = returnDate;
+        this.subscription = subscription;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Date getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(Date dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public boolean isSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(boolean subscription) {
+        this.subscription = subscription;
+    }
+}
