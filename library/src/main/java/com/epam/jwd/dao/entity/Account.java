e2f087bc5882bbdea8849a0aa09e760b3ff57c5f
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-16 12:29:10 +0300

    Add Account

---
+++ library/src/main/java/com/epam/jwd/dao/entity/Account.java
@@ -0,0 +1,44 @@
+package com.epam.jwd.dao.entity;
+
+public class Account extends Entity<Integer>{
+    private User user;
+    private String firstName;
+    private String secondName;
+
+    public Account(Integer id, User user, String firstName, String secondName) {
+        super(id);
+        this.user = user;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public Account(User user, String firstName, String secondName) {
+        this.user = user;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
