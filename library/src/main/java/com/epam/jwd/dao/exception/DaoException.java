47de8ceaf84e6d917876147d615a2b82de55fdca
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-18 23:59:56 +0300

    Change methods

--- library/src/main/java/com/epam/jwd/dao/exception/DaoException.java
+++ library/src/main/java/com/epam/jwd/dao/exception/DaoException.java
@@ -2,7 +2,6 @@ package com.epam.jwd.dao.exception;
 
 public class DaoException extends Exception {
     public DaoException() {
-        super();
     }
 
     public DaoException(String message) {
@@ -17,8 +16,8 @@ public class DaoException extends Exception {
         super(cause);
     }
 
-    protected DaoException(String message, Throwable cause, boolean enableSuppression,
-                           boolean writableStackTrace) {
+    public DaoException(String message, Throwable cause, boolean enableSuppression,
+                        boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
