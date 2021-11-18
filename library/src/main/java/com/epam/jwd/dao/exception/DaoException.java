877cb30a5093213764b2039e801f870f4cbb80de
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-18 20:19:37 +0300

    Add DaoException

---
+++ library/src/main/java/com/epam/jwd/dao/exception/DaoException.java
@@ -0,0 +1,24 @@
+package com.epam.jwd.dao.exception;
+
+public class DaoException extends Exception {
+    public DaoException() {
+        super();
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DaoException(String message, Throwable cause, boolean enableSuppression,
+                           boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
