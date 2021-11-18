f9ee3d995a3e32452cbad49932227654a33b87e4
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-18 20:10:08 +0300

    Upd BaseDao

--- library/src/main/java/com/epam/jwd/dao/api/BaseDao.java
+++ library/src/main/java/com/epam/jwd/dao/api/BaseDao.java
@@ -3,10 +3,11 @@ package com.epam.jwd.dao.api;
 import com.epam.jwd.dao.entity.Entity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BaseDao<T extends Entity<K>, K> {
     T add(T entity);
     T remove(T entity);
     List<T> findAll();
-    T findById(K id);
+    Optional<T> findById(K id);
 }
