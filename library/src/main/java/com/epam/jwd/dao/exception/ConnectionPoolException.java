e6d3eb083ea5a274e2588f8df56022a92af3219e
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-18 23:44:07 +0300

    Add ConnectionPoolException

---
+++ library/src/main/java/com/epam/jwd/dao/exception/ConnectionPoolException.java
@@ -0,0 +1,23 @@
+package com.epam.jwd.dao.exception;
+
+public class ConnectionPoolException extends Exception {
+    public ConnectionPoolException() {
+    }
+
+    public ConnectionPoolException(String message) {
+        super(message);
+    }
+
+    public ConnectionPoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConnectionPoolException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConnectionPoolException(String message, Throwable cause, boolean enableSuppression,
+                                   boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
