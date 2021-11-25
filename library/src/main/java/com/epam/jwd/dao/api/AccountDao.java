a4506aa6e3fc470e1520ed724c0d07ad416345af
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-25 18:25:53 +0300

    Upd AccountDao

--- library/src/main/java/com/epam/jwd/dao/api/AccountDao.java
+++ library/src/main/java/com/epam/jwd/dao/api/AccountDao.java
@@ -3,7 +3,9 @@ package com.epam.jwd.dao.api;
 import com.epam.jwd.dao.entity.user.Account;
 import com.epam.jwd.dao.exception.DaoException;
 
-public interface AccountDao extends BaseDao<Account, Integer> {
-    void createAccount(Account account) throws DaoException;
+import java.util.Optional;
 
+public interface AccountDao extends BaseDao<Account, Integer> {
+    Optional<Account> findAccountByUserId(Integer userId) throws DaoException;
+    void updatePhoneById(String phone, Integer id) throws DaoException;
 }
