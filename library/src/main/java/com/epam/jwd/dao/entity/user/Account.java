c06d5ae59dae253374dc7c6c11d05f58d6ca613f
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-25 16:18:44 +0300

    Replacement User on userId

--- library/src/main/java/com/epam/jwd/dao/entity/user/Account.java
+++ library/src/main/java/com/epam/jwd/dao/entity/user/Account.java
@@ -3,29 +3,29 @@ package com.epam.jwd.dao.entity.user;
 import com.epam.jwd.dao.entity.Entity;
 
 public class Account extends Entity<Integer> {
-    private User user;
+    private Integer userId;
     private String firstName;
     private String secondName;
 
-    public Account(Integer id, User user, String firstName, String secondName) {
+    public Account(Integer id, Integer userId, String firstName, String secondName) {
         super(id);
-        this.user = user;
+        this.userId = userId;
         this.firstName = firstName;
         this.secondName = secondName;
     }
 
-    public Account(User user, String firstName, String secondName) {
-        this.user = user;
+    public Account(Integer userId, String firstName, String secondName) {
+        this.userId = userId;
         this.firstName = firstName;
         this.secondName = secondName;
     }
 
-    public User getUser() {
-        return user;
+    public Integer getUserId() {
+        return userId;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 
     public String getFirstName() {
