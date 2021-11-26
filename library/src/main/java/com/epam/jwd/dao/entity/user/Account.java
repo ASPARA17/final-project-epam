0fee2a6f608e4c0e5e4138244c8dd21431263f5e
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-26 14:36:11 +0300

    Add AccountBuilder

--- library/src/main/java/com/epam/jwd/dao/entity/user/Account.java
+++ library/src/main/java/com/epam/jwd/dao/entity/user/Account.java
@@ -7,33 +7,20 @@ public class Account extends Entity<Integer> {
     private String firstName;
     private String secondName;
     private String phone;
+    private Integer subscriptionId;
 
-    public Account(Integer id, Integer userId, String firstName, String secondName, String phone) {
+    public Account(Integer id) {
+        super(id);
+    }
+
+    public Account(Integer id, Integer userId, String firstName, String secondName, String phone,
+                   Integer subscriptionId) {
         super(id);
         this.userId = userId;
         this.firstName = firstName;
         this.secondName = secondName;
         this.phone = phone;
-    }
-
-    public Account(Integer id, Integer userId, String firstName, String secondName) {
-        super(id);
-        this.userId = userId;
-        this.firstName = firstName;
-        this.secondName = secondName;
-    }
-
-    public Account(Integer userId, String firstName, String secondName, String phone) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.phone = phone;
-    }
-
-    public Account(Integer userId, String firstName, String secondName) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.secondName = secondName;
+        this.subscriptionId = subscriptionId;
     }
 
     public Integer getUserId() {
@@ -67,4 +54,66 @@ public class Account extends Entity<Integer> {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
+    public Integer getSubscriptionId() {
+        return subscriptionId;
+    }
+
+    public void setSubscriptionId(Integer subscriptionId) {
+        this.subscriptionId = subscriptionId;
+    }
+
+    public static class AccountBuilder {
+        private Integer id;
+        private Integer userId;
+        private String firstName;
+        private String secondName;
+        private String phone;
+        private Integer subscriptionId;
+
+        public AccountBuilder() {
+        }
+
+        public AccountBuilder withId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public AccountBuilder withUserId(Integer userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public AccountBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public AccountBuilder withSecondName(String secondName) {
+            this.secondName = secondName;
+            return this;
+        }
+
+        public AccountBuilder withPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public AccountBuilder withSubscriptionId(Integer subscriptionId) {
+            this.subscriptionId = subscriptionId;
+            return this;
+        }
+
+        public Account build() {
+            Account account = new Account(this.id);
+            account.setUserId(this.userId);
+            account.setFirstName(this.firstName);
+            account.setSecondName(this.secondName);
+            account.setPhone(this.phone);
+            account.setSubscriptionId(this.subscriptionId);
+            return account;
+        }
+    }
+
+
 }
