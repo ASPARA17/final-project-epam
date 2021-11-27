eea927b2f85979810d0d93a75dd99205e6aec94b
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-27 16:15:15 +0300

    Add new method

--- library/src/main/java/com/epam/jwd/dao/api/OrderDao.java
+++ library/src/main/java/com/epam/jwd/dao/api/OrderDao.java
@@ -3,11 +3,13 @@ package com.epam.jwd.dao.api;
 import com.epam.jwd.dao.entity.order.Order;
 import com.epam.jwd.dao.exception.DaoException;
 
+import java.util.Date;
 import java.util.List;
 
 public interface OrderDao extends BaseDao<Order, Integer>{
     List<Order> findByStatus(String status) throws DaoException;
-    void updateStatusById(Integer id) throws DaoException;
+    void updateStatusById(Integer statusId, Integer id) throws DaoException;
     List<Order> findAllByAccountId(Integer id) throws DaoException;
+    void updateReturnDateById(Date returnDate, Integer id) throws DaoException;
 
 }
