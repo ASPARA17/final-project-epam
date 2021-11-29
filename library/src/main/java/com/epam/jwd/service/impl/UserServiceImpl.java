49a2d5158f1769f8e14a2c2385f5047103929a3a
Sofya Prokopovich <sofya0804@gmail.com>
2021-11-29 20:26:56 +0300

    Add singleton

--- library/src/main/java/com/epam/jwd/service/impl/UserServiceImpl.java
+++ library/src/main/java/com/epam/jwd/service/impl/UserServiceImpl.java
@@ -1,12 +1,41 @@
 package com.epam.jwd.service.impl;
 
+import com.epam.jwd.dao.api.UserDao;
+import com.epam.jwd.dao.connection.api.ConnectionPool;
+import com.epam.jwd.dao.connection.impl.ConnectionPoolImpl;
+import com.epam.jwd.dao.entity.user.User;
+import com.epam.jwd.dao.impl.UserDaoImpl;
 import com.epam.jwd.service.api.UserService;
+import com.epam.jwd.service.converter.Converter;
+import com.epam.jwd.service.converter.impl.UserConverter;
 import com.epam.jwd.service.dto.userdto.UserDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+    private final Converter<User, UserDto, Integer> converter;
+    private static volatile UserServiceImpl instance;
+
+    private UserServiceImpl() {
+        this.userDao = UserDaoImpl.getInstance();
+        this.converter = new UserConverter();
+    }
+
+    public static UserServiceImpl getInstance() {
+        UserServiceImpl localInstance = instance;
+        if (instance == null) {
+            synchronized (UserServiceImpl.class) {
+                localInstance = instance;
+                if (instance == null) {
+                    instance = localInstance = new UserServiceImpl();
+                }
+            }
+        }
+        return  localInstance;
+    }
+
     @Override
     public UserDto add(UserDto entity) {
         return null;
