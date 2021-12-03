15fd3fdf3d4245ebba5666f50cdc9d9301e53986
Sofya Prokopovich <sofya0804@gmail.com>
2021-12-04 00:42:19 +0300

    Add implement new methods

--- library/src/main/java/com/epam/jwd/service/impl/AccountServiceImpl.java
+++ library/src/main/java/com/epam/jwd/service/impl/AccountServiceImpl.java
@@ -1,24 +1,83 @@
 package com.epam.jwd.service.impl;
 
+import com.epam.jwd.dao.api.AccountDao;
+import com.epam.jwd.dao.entity.user.Account;
+import com.epam.jwd.dao.exception.DaoException;
+import com.epam.jwd.dao.impl.AccountDaoImpl;
 import com.epam.jwd.service.api.AccountService;
+import com.epam.jwd.service.converter.Converter;
+import com.epam.jwd.service.converter.impl.AccountConverter;
 import com.epam.jwd.service.dto.userdto.AccountDto;
+import com.epam.jwd.service.exception.ServiceException;
+import com.epam.jwd.service.validator.AccountValidator;
+import com.epam.jwd.service.validator.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class AccountServiceImpl implements AccountService {
-    @Override
-    public AccountDto add(AccountDto entity) {
-        return null;
+    private final AccountDao accountDao;
+    private final Converter<Account, AccountDto, Integer> converter;
+    private final Validator<AccountDto, Integer> validator;
+    private static volatile AccountServiceImpl instance;
+
+    private AccountServiceImpl() {
+        this.accountDao = AccountDaoImpl.getInstance();
+        this.converter = new AccountConverter();
+        this.validator = new AccountValidator();
+    }
+
+    public static AccountServiceImpl getInstance() {
+        AccountServiceImpl localInstance = instance;
+        if (instance == null) {
+            synchronized (AccountServiceImpl.class) {
+                localInstance = instance;
+                if (instance == null) {
+                    instance = localInstance = new AccountServiceImpl();
+                }
+            }
+        }
+        return  localInstance;
     }
 
     @Override
-    public List<AccountDto> findAll() {
-        return null;
+    public AccountDto create(AccountDto accountDto) throws ServiceException {
+        validator.validate(accountDto);
+        Account createdAccount = converter.convert(accountDto);
+        try {
+            accountDto = converter.convert(accountDao.add(createdAccount));
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
+        return accountDto;
     }
 
     @Override
-    public Optional<AccountDto> findById(Integer id) {
-        return Optional.empty();
+    public List<AccountDto> findAll() throws ServiceException {
+        List<AccountDto> accountDto = new ArrayList<>();
+        try {
+            for (Account account : accountDao.findAll()) {
+                accountDto.add(converter.convert(account));
+            }
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
+        return accountDto;
+    }
+
+    @Override
+    public Optional<AccountDto> findById(Integer id) throws ServiceException {
+        Optional<AccountDto> accountDtoOptional = Optional.empty();
+        try {
+            Optional<Account> account = accountDao.findById(id);
+            if (account.isPresent()) {
+                AccountDto accountDto = converter.convert(account.get());
+                accountDtoOptional = Optional.of(accountDto);
+            }
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
+        return accountDtoOptional;
     }
 }
--- library/src/main/java/com/epam/jwd/service/impl/UserServiceImpl.java
+++ library/src/main/java/com/epam/jwd/service/impl/UserServiceImpl.java
@@ -1,26 +1,33 @@
 package com.epam.jwd.service.impl;
 
 import com.epam.jwd.dao.api.UserDao;
-import com.epam.jwd.dao.connection.api.ConnectionPool;
-import com.epam.jwd.dao.connection.impl.ConnectionPoolImpl;
 import com.epam.jwd.dao.entity.user.User;
+import com.epam.jwd.dao.exception.DaoException;
 import com.epam.jwd.dao.impl.UserDaoImpl;
 import com.epam.jwd.service.api.UserService;
 import com.epam.jwd.service.converter.Converter;
 import com.epam.jwd.service.converter.impl.UserConverter;
 import com.epam.jwd.service.dto.userdto.UserDto;
+import com.epam.jwd.service.exception.LoginNotUniqueException;
+import com.epam.jwd.service.exception.PasswordNotConfirmedException;
+import com.epam.jwd.service.exception.ServiceException;
+import com.epam.jwd.service.validator.UserValidator;
+import com.epam.jwd.service.validator.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
     private final Converter<User, UserDto, Integer> converter;
+    private final Validator<UserDto, Integer> validator;
     private static volatile UserServiceImpl instance;
 
     private UserServiceImpl() {
         this.userDao = UserDaoImpl.getInstance();
         this.converter = new UserConverter();
+        this.validator = new UserValidator();
     }
 
     public static UserServiceImpl getInstance() {
@@ -37,28 +44,61 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDto add(UserDto entity) {
-        return null;
+    public UserDto create(UserDto userDto) throws ServiceException, LoginNotUniqueException {
+        validator.validate(userDto);
+        User createdUser = converter.convert(userDto);
+        try {
+            checkLoginUnique(createdUser.getLogin());
+            userDto = converter.convert(userDao.add(createdUser));
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
+        return userDto;
     }
 
     @Override
-    public List<UserDto> findAll() {
-        return null;
+    public List<UserDto> findAll() throws ServiceException {
+        List<UserDto> usersDto = new ArrayList<>();
+        try {
+            for (User user : userDao.findAll()) {
+                usersDto.add(converter.convert(user));
+            }
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
+        return usersDto;
     }
 
     @Override
-    public Optional<UserDto> findById(Integer id) {
-        return Optional.empty();
+    public Optional<UserDto> findById(Integer id) throws ServiceException {
+        Optional<UserDto> userDtoOptional = Optional.empty();
+        try {
+           Optional<User> user = userDao.findById(id);
+           if (user.isPresent()) {
+               UserDto userDto = converter.convert(user.get());
+               userDtoOptional = Optional.of(userDto);
+           }
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
+        return userDtoOptional;
     }
 
-    @Override
-    public Optional<UserDto> sigInUser(String login, String password) {
-        return Optional.empty();
+    public void verifyPassword(String password, String confirmPassword) throws PasswordNotConfirmedException {
+        if (!password.equals(confirmPassword)) {
+            // TODO: add msg
+            throw new PasswordNotConfirmedException();
+        }
     }
 
-    @Override
-    public UserDto registerUser(String login, String password, String confirmPassword,
-                                String firstName, String secondName, String phone) {
-        return null;
+    private void checkLoginUnique(String login) throws LoginNotUniqueException, ServiceException {
+        // TODO: add msg
+        try {
+            converter.convert(userDao
+                    .findByLogin(login)
+                    .orElseThrow(LoginNotUniqueException::new));
+        } catch (DaoException e) {
+            throw new ServiceException();
+        }
     }
 }
