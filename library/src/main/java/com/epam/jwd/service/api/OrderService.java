51829e4b726900704b4ef760907d3919e9b06e5b
Sofya Prokopovich <sofya0804@gmail.com>
2021-12-04 00:49:15 +0300

    Add new method

--- library/src/main/java/com/epam/jwd/service/api/OrderService.java
+++ library/src/main/java/com/epam/jwd/service/api/OrderService.java
@@ -1,7 +1,8 @@
 package com.epam.jwd.service.api;
 
 import com.epam.jwd.service.dto.orderdto.OrderDto;
+import com.epam.jwd.service.exception.ServiceException;
 
 public interface OrderService extends Service<OrderDto, Integer> {
-
+    OrderDto create(OrderDto orderDto) throws ServiceException;
 }
--- library/src/main/java/com/epam/jwd/service/impl/OrderServiceImpl.java
+++ library/src/main/java/com/epam/jwd/service/impl/OrderServiceImpl.java
@@ -2,23 +2,24 @@ package com.epam.jwd.service.impl;
 
 import com.epam.jwd.service.api.OrderService;
 import com.epam.jwd.service.dto.orderdto.OrderDto;
+import com.epam.jwd.service.exception.ServiceException;
 
 import java.util.List;
 import java.util.Optional;
 
 public class OrderServiceImpl implements OrderService {
     @Override
-    public OrderDto add(OrderDto entity) {
+    public OrderDto create(OrderDto orderDto) throws ServiceException {
         return null;
     }
 
     @Override
-    public List<OrderDto> findAll() {
+    public List<OrderDto> findAll() throws ServiceException {
         return null;
     }
 
     @Override
-    public Optional<OrderDto> findById(Integer id) {
+    public Optional<OrderDto> findById(Integer id) throws ServiceException {
         return Optional.empty();
     }
 }
