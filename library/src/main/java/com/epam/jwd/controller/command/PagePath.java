cac2e5664b6a87e67a7e6b5626e0da1dcf51c884
Sofya Prokopovich <sofya0804@gmail.com>
2021-12-05 03:13:52 +0300

    Add private constructor for safe

--- library/src/main/java/com/epam/jwd/controller/command/PagePath.java
+++ library/src/main/java/com/epam/jwd/controller/command/PagePath.java
@@ -3,7 +3,7 @@ package com.epam.jwd.controller.command;
 public class PagePath {
     private PagePath() {
     }
-    
+
     public static final String MAIN_PAGE_PATH = "WEB-INF/jsp/main.jsp";
     public static final String LOGIN_PAGE_PATH = "WEB-INF/jsp/login.jsp";
     public static final String USER_HOME_PAGE_PATH = "WEB-INF/jsp/";
