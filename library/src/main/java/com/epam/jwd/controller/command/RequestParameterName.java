1450590574c23aef83ca3f544e1cf32df0719f08
Sofya Prokopovich <sofya0804@gmail.com>
2021-12-09 22:23:48 +0300

    Add class with request param name

---
+++ library/src/main/java/com/epam/jwd/controller/command/RequestParameterName.java
@@ -0,0 +1,12 @@
+package com.epam.jwd.controller.command;
+
+public class RequestParameterName {
+    public static final String LOGIN = "login";
+    public static final String PASSWORD = "password";
+    public static final String LANGUAGE = "language";
+
+
+
+    private RequestParameterName() {
+    }
+}
