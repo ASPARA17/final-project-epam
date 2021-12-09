7edf288807c279c12a9d27b6afdb7883268e39fa
Sofya Prokopovich <sofya0804@gmail.com>
2021-12-09 22:23:25 +0300

    Add new page path

--- library/src/main/java/com/epam/jwd/controller/command/PagePath.java
+++ library/src/main/java/com/epam/jwd/controller/command/PagePath.java
@@ -1,11 +1,15 @@
 package com.epam.jwd.controller.command;
 
 public class PagePath {
-    private PagePath() {
-    }
-
     public static final String MAIN_PAGE_PATH = "WEB-INF/jsp/main.jsp";
-    public static final String LOGIN_PAGE_PATH = "WEB-INF/jsp/login.jsp";
+    public static final String LOGIN_PAGE_PATH = "WEB-INF/jsp/signIn.jsp";
     public static final String USER_HOME_PAGE_PATH = "WEB-INF/jsp/";
     public static final String DEFAULT_PAGE_PATH = "WEB-INF/jsp/main.jsp";
+    public static final String ERROR_500 = "WEB-INF/jsp/error500.jsp";
+    public static final String ABOUT_LIBRARY_PAGE = "WEB-INF/jsp/aboutLibrary.jsp";
+    public static final String CATALOG_PAGE_PATH = "WEB-INF/jsp/catalog.jsp";
+    public static final String REGISTRATION_PAGE_PATH = "WEB-INF/jsp/registration.jsp";
+
+    private PagePath() {
+    }
 }
